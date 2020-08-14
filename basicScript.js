f0ba1192c1fb332ed424b7364cacf5b42450ee64
Serhii Kambur <sergey.2000.kambur@gmail.com>
2020-08-14 21:39:29 +0300

    updated - now on 2.8 learn.js

--- basicScript.js
+++ basicScript.js
@@ -1,8 +1,24 @@
+'use strict' 
 const firstName = 'Sergey'
-const age = 20
+//const age = 20
 const isProgrammer = true
 
 //console.log('Имя человека: ' + firstName + ' а возраст человека:' + age.toString())
 //console.log(age)
-const lastName = prompt('Введите фамилию')
-alert(firstName + ' ' + lastName)
+//const lastName = prompt('Введите фамилию')
+//alert(firstName + ' ' + lastName)
+
+const currentYear = 2020
+const birthYear = 2000
+
+//const age= currentYear - birthYear
+
+const a = 10
+const b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+
---
+++ learnJS.js
@@ -0,0 +1,10 @@
+"use strict"
+let a=1
+let b=2
+let c=3
+a=b=c=5+1
+console.log(a + ' ' +  b + ' ' + c)
+
+1
+-1
+-1
